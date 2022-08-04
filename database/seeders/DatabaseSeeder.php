def98f8a8895172c86c7de506378dd40fd6e79b2
okonueugene <tylarrex67@gmail.com>
2022-08-04 15:03:47 +0300

    Api token

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,46 @@ class DatabaseSeeder extends Seeder
             'email' => 'user@example.com',
             'password' =>'thispassword',
         ]);
+        $user1 = User::create([
+            'name' => 'This User1', 
+            'email' => 'user1@example.com',
+            'password' =>'thispassword',
+        ]);
+        $user2 = User::create([
+            'name' => 'This User2', 
+            'email' => 'user2@example.com',
+            'password' =>'thispassword',
+        ]);
+        $user3 = User::create([
+            'name' => 'This User3', 
+            'email' => 'user3@example.com',
+            'password' =>'thispassword',
+        ]);
+        $user4 = User::create([
+            'name' => 'This User4', 
+            'email' => 'user4@example.com',
+            'password' =>'thispassword',
+        ]);
+        $user5 = User::create([
+            'name' => 'This User5', 
+            'email' => 'user5@example.com',
+            'password' =>'thispassword',
+        ]);
+        $user6 = User::create([
+            'name' => 'This User6', 
+            'email' => 'user6@example.com',
+            'password' =>'thispassword',
+        ]);
+        $user7 = User::create([
+            'name' => 'This User7', 
+            'email' => 'user7@example.com',
+            'password' =>'thispassword',
+        ]);
+        $user8 = User::create([
+            'name' => 'This User8', 
+            'email' => 'user8@example.com',
+            'password' =>'thispassword',
+        ]);
 
     }
 }
