79cc26ce5fda7ee2bbffa169ea887cd3dc32f3ed
okonueugene <tylarrex67@gmail.com>
2022-08-04 13:13:09 +0300

    Api token

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,23 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
-
-        \App\Models\User::factory()->create([
-            'name' => 'Testi User',
-            'email' => 'testi@example.com',
-            'password'=>'12345678'
+        $user = User::create([
+            'name' => 'Felix Mutinda', 
+            'email' => 'admin@admin.com',
+            'password' =>'thispassword',
         ]);
+
     }
 }
